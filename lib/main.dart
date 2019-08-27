364459242c3c581a1df1e617c808462974be444e
Gwen <gwendolyn.rippberger@gmail.com>
2019-08-27 14:00:26 +0200

    fight working

--- lib/main.dart
+++ lib/main.dart
@@ -22,59 +22,117 @@ class MyApp extends StatelessWidget {
 }
 
 class HeroState extends State<HeroList> {
-  Hero hero = null;
-  final _heros = <Hero>[];
+  Future<List<Hero>> heroesFuture;
+  Set<Hero> fighter = Set<Hero>();
   final _biggerFont = const TextStyle(fontSize: 20.0);
 
   @override
-  initState() {
+  void initState() {
     super.initState();
-    generateHero().then((Hero value) {
-      setState(() {
-        hero = value;
-      });
-  });
+    heroesFuture = generateHeroes();
   }
 
   @override
   Widget build(BuildContext context) {
-    if(hero == null)  {
-      return Text(
-        'Loading...',
-        style: _biggerFont,
-      );
-    } else {
-      return Scaffold(
-        appBar: AppBar(
-          title: Text('Hero List'),
-        ),
-        body: _buildHeros(),
-      );
-    }
+    return Scaffold(
+      appBar: AppBar(title: Text('Heroes (Choose two Heroes to fight)', style: TextStyle(fontSize: 15.0)), actions: <Widget>[
+        new FlatButton(
+          child: Text(
+            "Fight",
+            style: const TextStyle(fontSize: 20.0, color: Colors.white),
+          ),
+          onPressed: _showFight,
+        )
+      ]),
+      body: _buildHeroes(),
+    );
   }
 
-  Widget _buildHeros() {
-    return ListView.builder(
-        padding: const EdgeInsets.all(16.0),
-        itemBuilder: (context, i) {
-          if (i.isOdd) return Divider();
+  void _showFight() {
+    showDialog<Null>(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context) {
+        return new AlertDialog(
+          title: new Text('Hero Fight'),
+          content: new SingleChildScrollView(
+            child: new ListBody(
+              children: <Widget>[
+                new Text(fighter.elementAt(0).fight(fighter.elementAt(1))),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            new FlatButton(
+              child: new Text('Ok'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
 
-          final index = i ~/ 2;
-          if (index >= _heros.length) {
-             generateHero().then((Hero value) {
-               _heros.add(value);
-             });
-          }
-          return _buildRow(_heros[index]);
-        });
+  Widget _buildHeroes() {
+    return FutureBuilder(
+      future: heroesFuture,
+      builder: (context, snapshot) {
+        switch (snapshot.connectionState) {
+          case ConnectionState.none:
+          case ConnectionState.waiting:
+          case ConnectionState.active:
+            return Container(
+              alignment: Alignment.center,
+              child: Text("Loading..."),
+            );
+            break;
+          case ConnectionState.done:
+            if (snapshot.hasError) {
+              return Container(
+                alignment: Alignment.center,
+                child: Text("Error: ${snapshot.error}"),
+              );
+            }
+            var data = snapshot.data;
+            return new ListView.builder(
+              padding: const EdgeInsets.all(16.0),
+              reverse: false,
+              itemBuilder: (_, int index) => _buildRow(data[index]),
+              itemCount: data.length,
+            );
+            break;
+        }
+      },
+    );
   }
 
   Widget _buildRow(Hero hero) {
+    final bool alreadySaved = fighter.contains(hero);
     return ListTile(
-      title: Text(
-        hero.name,
-        style: _biggerFont,
+      leading: ClipOval(
+        child: Image.network(
+          hero.image,
+          width: 70,
+          height: 50,
+          fit: BoxFit.cover,
+        ),
       ),
+      title: Text(hero.name, style: _biggerFont),
+        trailing: Icon(   // Add the lines from here...
+          alreadySaved ? Icons.add_circle : Icons.add_circle_outline,
+          color: alreadySaved ? Colors.black : null,
+        ),
+      onTap: () {      // Add 9 lines from here...
+        setState(() {
+          if (alreadySaved) {
+            fighter.remove(hero);
+          } else if (fighter.length < 2) {
+            fighter.add(hero);
+          }
+        });
+      },
     );
   }
 }
@@ -86,40 +144,58 @@ class HeroList extends StatefulWidget {
 
 Future<Hero> generateHero() async {
   int randomHero = new Random().nextInt(731);
-  final response =
-  await http.get(url+'/'+randomHero.toString());
+  final response = await http.get(url + '/' + randomHero.toString());
 
   if (response.statusCode == 200) {
-    // If server returns an OK response, parse the JSON.
     return await Hero.fromJson(json.decode(response.body));
   } else {
-    // If that response was not OK, throw an error.
     throw await Exception('Failed to load post');
   }
 }
 
-class Hero {
-  //final List<int> powerstats;
-  final String name;
+Future<List<Hero>> generateHeroes() async {
+  List<Hero> heroes = <Hero>[];
+  for (int i = 0; i <= 20; i++) {
+    int randomHero = new Random().nextInt(731);
+    final response = await http.get(url + '/' + randomHero.toString());
 
-  Hero({this.name});
-
-  /*
-  Widget fight(Hero opponent) {
-    int randomPower = new Random().nextInt(5);
-    if(this.powerstats[randomPower] > opponent.powerstats[randomPower]) {
-      return Text(this.name + ' won the fight!');
+    if (response.statusCode == 200) {
+      heroes.add(await Hero.fromJson(json.decode(response.body)));
     } else {
-      return Text(opponent.name + ' won the fight!');
+      throw await Exception('Failed to load post');
     }
-  }*/
-
-  factory Hero.fromJson(Map<String, dynamic> json) {
-    return Hero(
-        name: json['name'],
-        //powerstats: json['powerstats']
-    );
   }
+  return heroes;
 }
 
+class Hero {
+  final String name;
+  final String image;
+  final dynamic powerstats;
 
+  Hero({this.name, this.image, this.powerstats});
+
+  String fight(Hero opponent) {
+    int randomPower = new Random().nextInt(5);
+    List<String> keys = this.powerstats.keys.toList();
+    String power = keys[randomPower];
+    if (int.parse(this.powerstats[power]) >
+        int.parse(opponent.powerstats[power])) {
+      return this.name + ' won the fight! With '+this.powerstats[power]+' '+power;
+    } else {
+      return opponent.name + ' won the fight! With '+opponent.powerstats[power]+' '+power;
+    }
+  }
+
+  factory Hero.fromJson(Map<String, dynamic> json) {
+    var imageMap = json['image'].values.toList();
+    var imageUrl = imageMap[0];
+
+    if (imageUrl == null) {
+      imageUrl = 'https://via.placeholder.com/150';
+    }
+
+    var powerstatsMap = json['powerstats'];
+    return Hero(name: json['name'], image: imageUrl, powerstats: powerstatsMap);
+  }
+}
